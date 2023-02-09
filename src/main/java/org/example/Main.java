06b0219d864a750e2f01582e5a20aafab655128b
samil <samilcd.26@outlook.com>
2023-02-09 22:11:34 +0300

    New question solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,37 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> test=Reverser.StringTolist("5 9 10 7 4");
+      Scanner scan = new Scanner(System.in);
+      int inputNumber=scan.nextInt();
+      List<Integer> AllStudent=new ArrayList<>();
 
-        int n=6 ;
-        int k=3;
-        List<Integer> ar =List.of(1, 3, 2, 6, 1, 2);
-        System.out.println(divisibleSumPairs(n,k,ar));
+      for(int i=0;i<inputNumber;i++){
+          int not =scan.nextInt();
+          AllStudent.add(not);
+      }
+
+
+
+        System.out.println(gradingStudents(AllStudent));
     }
 
-    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
-        int score=0;
-        for (int x=0;x<n;x++){
-            for (int y=n-1;y>x;y--){
-                if ((ar.get(x)+ar.get(y))%k==0) score++;
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        List<Integer> returnData=new ArrayList<>();
 
-            }
+
+        for (int not:grades) {
+            if(not<38) returnData.add(not);
+            else if (not%10==2 ||not%10==1||not%10==6|| not%10==7) returnData.add(not);
+            else if (not%10>5 && (10-not%10)<=3) returnData.add(not+(10-not%10));
+            else if(not%10<5 && (5-not%10)<=3)returnData.add(not+(5-not%10));
+            else returnData.add(not);
         }
-    return score;
+
+        return returnData;
     }
 
-//    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
-//        int score=0;
-//        for (int x=0;x<n;x++){
-//            for (int y=1;y<n;y++){
-//                if ((ar.get(x)+ar.get(y))%k==0) System.out.println(ar.get(x)+" + "+ar.get(y));
-//                if (y<x)y+=x;
-//
-//            }
-//        }
-//        return score-1;
-//    }
 }
 
